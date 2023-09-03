ebd94586ef741430d84c00c577f38d93c2c2a942
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-03 15:12:01 +0300

    xpgold

---
+++ Week2/Day1/XP Gold/xpgold.py
@@ -0,0 +1,36 @@
+# Exercise 1 : Hello World-I Love Python
+# Instructions
+# Print the following output in one line of code:
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# I love python
+# I love python
+# I love python
+# I love python
+
+# print("Hello world\n"*4 + "I love python\n"*4) 
+
+# Exercise 2 : What Is The Season ?
+# Instructions
+# Ask the user to input a month (1 to 12).
+# Display the season of the month received :
+# Spring runs from March (3) to May (5)
+# Summer runs from June (6) to August (8)
+# Autumn runs from September (9) to November (11)
+# Winter runs from December (12) to February (2)
+
+# value = int((input("Type in number of the month \n")))
+# if 3 <= value <= 5:
+#     print("It's spring") 
+# elif 6 <= value <= 8:
+#     print("It's summer") 
+# elif 9 <= value <= 11:
+#     print("It's autumn") 
+# elif value == 12 or value == 1 or value == 2:
+#     print("It's winter") 
+# else: 
+#     print("Error")
+
+
