5bc13db6f7a55af57815f1fa749af35ae20a5c1f
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-04 22:09:48 +0300

    dc 1

---
+++ Week2/Day2/dc/dc.py
@@ -0,0 +1,14 @@
+#1
+
+# user_input = input("Insert a number and length")
+# list_user = user_input.split(",") 
+# list_user = [int(x) for x in list_user]
+# number = list(range(1, list_user[1] + 1))
+# row = []
+# for i in number:
+#     new = list_user[0] * i  
+#     row.append(new)
+# print(row)
+
+#2
+
