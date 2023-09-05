4ab2bf75caefd5e11b3636f6b40c399f4cbb4817
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-05 13:47:20 +0300

    xp start

---
+++ Week2/Day3/ExcercisesXP/xp.py
@@ -0,0 +1,108 @@
+# Exercise 1 : Convert Lists Into Dictionaries
+# Instructions
+# Convert the two following lists, into dictionaries.
+# Hint: Use the zip method
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+# Expected output:
+# {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+# output = list(zip(keys, values))
+# dict1 = dict(output)
+# print(dict1)
+
+# Exercise 2 : Cinemax
+# Instructions
+# A movie theater charges different ticket prices depending on a person’s age.
+# if a person is under the age of 3, the ticket is free.
+# if they are between 3 and 12, the ticket is $10.
+# if they are over the age of 12, the ticket is $15.
+
+# Given the following object:
+
+# family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+
+
+# How much does each family member have to pay ?
+
+# Print out the family’s total cost for the movies.
+# Bonus: Ask the user to input the names and ages instead of using the provided family variable (Hint: ask the user for names and ages and add them into a family dictionary that is initially empty).
+
+# family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+# price2 = []
+# for x, y in family.items():
+#     if y<3: 
+#         price2.append(0)
+#     elif 3<y<12:
+#         price2.append(10)
+#     elif y>12:
+#         price2.append(15)
+
+# total_cost = sum(price2)
+# print(f"The family's total cost for the movies is {total_cost}")
+
+#bonus question
+# family = {}
+# for i in range (4):
+#     x = input("Enter the name")
+#     y = int(input("Enter the age"))
+#     family[x] = y
+# price2 = []
+# for x, y in family.items():
+#     if y<3: 
+#         price2.append(0)
+#     elif 3<y<12:
+#         price2.append(10)
+#     elif y>12:
+#         price2.append(15)
+
+# total_cost = sum(price2)
+# print(f"The family's total cost for the movies is {total_cost}")
+
+# Exercise 3: Zara
+# Instructions
+# Here is some information about a brand.
+# name: Zara 
+# creation_date: 1975 
+# creator_name: Amancio Ortega Gaona 
+# type_of_clothes: men, women, children, home 
+# international_competitors: Gap, H&M, Benetton 
+# number_stores: 7000 
+# major_color: 
+#     France: blue, 
+#     Spain: red, 
+#     US: pink, green
+
+
+# 2. Create a dictionary called brand which value is the information from part one (turn the info into keys and values).
+# The values type_of_clothes and international_competitors should be a list. The value of major_color should be a dictionary.
+# 3. Change the number of stores to 2.
+# 4. Print a sentence that explains who Zaras clients are.
+# 5. Add a key called country_creation with a value of Spain.
+# 6. Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
+# 7. Delete the information about the date of creation.
+# 8. Print the last international competitor.
+# 9. Print the major clothes colors in the US.
+# 10. Print the amount of key value pairs (ie. length of the dictionary).
+# 11. Print the keys of the dictionary.
+# 12. Create another dictionary called more_on_zara with the following details:
+
+# creation_date: 1975 
+# number_stores: 10 000
+
+
+# 13. Use a method to add the information from the dictionary more_on_zara to the dictionary brand.
+# 14. Print the value of the key number_stores. What just happened ?
+
+name: Zara 
+creation_date: 1975 
+creator_name: Amancio Ortega Gaona 
+type_of_clothes: men, women, children, home 
+international_competitors: Gap, H&M, Benetton 
+number_stores: 7000 
+major_color: 
+    France: blue, 
+    Spain: red, 
+    US: pink, green
--- Week2/Day3/main.py
+++ Week2/Day3/main.py
@@ -1,48 +1,136 @@
 # fruits = ["apple", "banana", "orange"]
 #dictionary - data type to save key value pairs
 
-fruits = {
-    "apple" : 2,
-    "banana" : 5,
-    "orange" : 1
-}
+# fruits = {
+#     "apple" : 2,
+#     "banana" : 5,
+#     "orange" : 1
+# }
 
-amount_apple = fruits ["apple"]
+# amount_apple = fruits ["apple"]
 # print(amount_apple) - prints the amount of apples
 
-total_amount_fruits = fruits["apple"] + fruits["banana"] + fruits["orange"]
+# total_amount_fruits = fruits["apple"] + fruits["banana"] + fruits["orange"]
 # print(total_amount_fruits)
 
 # fruits["orange"] = 2 - how to change the value in dictionary
 # print(fruits)
 
 #how better change the data in dictionary:
-fruits["orange"] += 1
+# fruits["orange"] += 1
 # print(fruits)
 
-family = {
-    "name" : "Smith",
-    "members" : 5,
-    "number_rooms" : 5,
-    "names" : ["Jon", "Paul"],
-    "live_in_tlv" : True,
-    "friends" : {
-        "username" : ["Sara", "Lea"],
-        "have_dog" : False
-    }
-}
+# family = {
+#     "name" : "Smith",
+#     "members" : 5,
+#     "number_rooms" : 5,
+#     "names" : ["Jon", "Paul"],
+#     "live_in_tlv" : True,
+#     "friends" : {
+#         "username" : ["Sara", "Lea"],
+#         "have_dog" : False
+#     }
+# }
 
-rooms = family["number_rooms"]
-second_members = family["names"][1]
-first_friend = family["friends"]["username"][0]
+# rooms = family["number_rooms"]
+# second_members = family["names"][1]
+# first_friend = family["friends"]["username"][0]
 # print(first_friend)
 
-price = {
+#excercise 1
+# price = {
+#     "apple" : 2,
+#     "banana" : 5,
+#     "orange" : 1
+# }
+
+# user_input = input("Type in your fruit")
+# price = price[user_input]
+# print(f"Your price is {price}")
+
+
+# exercise 2
+# create a new list that only contains the uppercased words
+# words = ['PYTHON', 'JOHN', 'chEEse', 'hAm', 'DOE', '123']
+# new_list = [word for word in words if str(word).isupper()]
+# print(new_list)
+
+
+fruit_amount = {
     "apple" : 2,
     "banana" : 5,
     "orange" : 1
 }
 
-user_input = input("Type in your fruit")
-price = price[user_input]
-print(f"Your price is {price}")
+# print(fruit_amount.keys())
+# print(fruit_amount.values())
+# print(fruit_amount.items())
+
+# total = 0
+# for amount in fruit_amount.values() : 
+#     total += amount
+# print(total)
+
+# for element in fruit_amount.items() :
+#     # print(element) #tuple
+#     print(f"the user bought {element[1]} {element[0]}")
+
+# for fruit,amount in fruit_amount.items() :
+#     if amount > 1: 
+#         print(f"the User bought {amount} {fruit}s")
+#     else:
+#         print(f"the User bought {amount} {fruit}")
+
+
+
+# Exercise 3
+# Print the total duration of the playlist
+
+playlist = {
+    'title': "Hello World",
+    'author': "Planet",
+    'songs': [
+        {
+            'song_title': "Song One",
+            'artist': ['Artist 1', 'Artist 2'],
+            'duration': 4.31,
+        },
+        {
+            'song_title': "Song Two",
+            'artist': ['Artist 1'],
+            'duration': 2.53,
+        },
+        {
+            'song_title': "Song Three",
+            'artist': ['Artist 1', 'Artist 2', 'Artist 3'],
+            'duration': 3.43,
+        }
+    ]
+}
+
+# total = 0
+# for music in playlist["songs"]:
+#     convert_to_seconds = music["duration"]
+#     total  += music["duration"]
+# print(total)
+
+
+
+# fruits = ["apple", "pear", "banana"]
+
+# for num in range(len(fruits)):
+#     fruits[num] += "s"
+
+# for index, value in enumerate(fruits):
+#     fruits[index] += "s"
+# print(fruits)
+
+# print(list(enumerate(fruits)))
+
+# list1 = ["apple", "banana", "pear"]
+# list2 = [1.2, 0.5, 1.6]
+# list3 = list(zip(list1, list2))
+# dict1 = dict(list3)
+# print(dict1)
+
+
