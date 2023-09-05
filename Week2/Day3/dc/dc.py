deaeb4572c00a3aa3e354dffae196f2e89ad17e9
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-05 21:45:07 +0300

    dc

---
+++ Week2/Day3/dc/dc.py
@@ -0,0 +1,14 @@
+#1
+
+# user_input = input("Insert a word:")
+# index_dict = {}
+# for index, item in enumerate(user_input):
+#     if item not in index_dict:
+#         index_dict[item] = [index]
+#     else:
+#         index_dict[item].append(index)
+# print(index_dict)
+
+
+#2
+
