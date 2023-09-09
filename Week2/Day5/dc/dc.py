2de503d636080161863bb4104a79a2bd0d1f7df4
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-09 13:43:17 +0300

    dc

--- Week2/Day5/dc/dc.py
+++ Week2/Day5/dc/dc.py
@@ -1,12 +1,21 @@
 #1
 
-def sort_sequence():
-    user_input = input("Type a sequence of words: \n")
-    sequence_list = []
-    sequence_list.extend(user_input.split(","))
-    result = sorted(sequence_list)
-    return result
+# def sort_sequence():
+#     user_input = input("Type a sequence of words: \n")
+#     sequence_list = []
+#     sequence_list.extend(user_input.split(","))
+#     result = sorted(sequence_list)
+#     return result
 
-print(sort_sequence())
+# print(sort_sequence())
 
+#2
+
+# def longest_word(user_input):
+#     sentence_list = user_input.split(" ")
+#     longest_word = max(sentence_list, key = len)
+#     print(f"The longest word in your sentence is {longest_word}")
+
+# user_input = input("Type your sentence: \n")
+# longest_word(user_input)
 
