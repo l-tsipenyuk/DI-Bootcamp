9cfd2e7fa2663b280747bd106c811ba64a69f546
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-27 16:06:04 +0300

    project

--- Week4/Day5/hackathon/main.py
+++ Week4/Day5/hackathon/main.py
@@ -94,48 +94,59 @@ class EnergyData:
 # methods to get the peers WORK ON THIS
 
     def get_the_peer(self, country, year, fuel_type):
-        total_gen_data = {}
-        fuel_type_gen_data = {}
+        country_list = ["Canada", "Mexico", "US", "Argentina", "Brazil", "France", "Germany", "Italy", "Netherlands", "Poland", "Spain", "Turkey", "Ukraine", "United Kingdom", "Kazakhstan", "Russian Federation", "Iran", "Saudi Arabia", "United Arab Emirates", "Egypt", "Australia", "China", "India", "Indonesia", "Japan", "Malaysia", "South Korea", "Taiwan", "Thailand", "Vietnam"]
+        
+        for i in country_list:
+            a = EnergyData(i)
+            a.histdata(year,year)
+        # table2 = self.get_the_share(country_list, year)
+        # return 
+ 
 
-        year_column_total = None
-        for col_index, cell in enumerate(ws_total[3]):
-            if cell.value == year:
-                year_column_total = col_index + 1  
-                break
 
-        year_column_hydro = None
-        for col_index, cell in enumerate(ws_hydro[3]):
-            if cell.value == year:
-                year_column_hydro = col_index + 1  
-                break
 
-        if year_column_total is not None and year_column_hydro is not None:
-            for row_total, row_hydro in zip(
-                ws_total.iter_rows(min_row=4, max_row=109, min_col=year_column_total, max_col=year_column_total),
-                ws_hydro.iter_rows(min_row=4, max_row=109, min_col=year_column_hydro, max_col=year_column_hydro)
-            ):
-                country_cell_total = row_total[0]
-                country_cell_hydro = row_hydro[0]
-                total_gen = country_cell_total.value
-                fuel_gen = country_cell_hydro.value
+        # total_gen_data = {}
+        # fuel_type_gen_data = {}
 
-                if total_gen is not None and fuel_gen is not None:
-                    total_gen_data[country_cell_total.value] = total_gen
-                    fuel_type_gen_data[country_cell_hydro.value] = fuel_gen
+        # year_column_total = None
+        # for col_index, cell in enumerate(ws_total[3]):
+        #     if cell.value == year:
+        #         year_column_total = col_index + 1  
+        #         break
 
-        share_data = {}
-        for country in total_gen_data:
-            total_gen = total_gen_data[country]
-            fuel_type_gen = fuel_type_gen_data.get(country, 0)
+        # year_column_hydro = None
+        # for col_index, cell in enumerate(ws_hydro[3]):
+        #     if cell.value == year:
+        #         year_column_hydro = col_index + 1  
+        #         break
 
-            if total_gen is not None and total_gen != 0:
-                share_data[country] = (fuel_type_gen / total_gen) * 100
+        # if year_column_total is not None and year_column_hydro is not None:
+        #     for row_total, row_hydro in zip(
+        #         ws_total.iter_rows(min_row=4, max_row=109, min_col=year_column_total, max_col=year_column_total),
+        #         ws_hydro.iter_rows(min_row=4, max_row=109, min_col=year_column_hydro, max_col=year_column_hydro)
+        #     ):
+        #         country_cell_total = row_total[0]
+        #         country_cell_hydro = row_hydro[0]
+        #         total_gen = country_cell_total.value
+        #         fuel_gen = country_cell_hydro.value
 
-        country_share = share_data.get(self.country, 0)
-        sorted_countries = sorted(share_data.items(), key=lambda x: abs(x[1] - country_share))
-        result = sorted_countries[:5]
+        #         if total_gen is not None and fuel_gen is not None:
+        #             total_gen_data[country_cell_total.value] = total_gen
+        #             fuel_type_gen_data[country_cell_hydro.value] = fuel_gen
 
-        print(result)
+        # share_data = {}
+        # for country in total_gen_data:
+        #     total_gen = total_gen_data[country]
+        #     fuel_type_gen = fuel_type_gen_data.get(country, 0)
+
+        #     if total_gen is not None and total_gen != 0:
+        #         share_data[country] = (fuel_type_gen / total_gen) * 100
+
+        # country_share = share_data.get(self.country, 0)
+        # sorted_countries = sorted(share_data.items(), key=lambda x: abs(x[1] - country_share))
+        # result = sorted_countries[:5]
+
+        # print(year_column_total)
 
         # for country, share in result:
         #     print(f"Country: {country}, Share, %: {share} ")
@@ -221,11 +232,37 @@ class EnergyData:
  
  
 
-a = EnergyData('Germany')
+# a = EnergyData('Germany')
 # print(a.get_the_share('Germany', 1995))
-print(a.get_the_peer('Germany', 2022, 'Hydro'))
+# print(a.get_the_peer('Germany', 2022, 'Hydro'))
 # a.connect_to_postgress()
 # print(a.create_a_table('energy_mix_ger', 2015, 2022))
 # print(a.add_data_to_a_table('energy_mix_ger', 2015, 2022))
 # print(a.delete_the_table('energy_mix_ger'))
+
+
+
+
+# country_list = ["Germany", "France"]
+country_list = ["Canada", "Mexico", "US", "Argentina", "Brazil", "France", "Germany", "Italy", "Netherlands", "Poland", "Spain", "Turkey", "Ukraine", "United Kingdom", "Kazakhstan", "Russian Federation", "Iran", "Saudi Arabia", "United Arab Emirates", "Egypt", "Australia", "China", "India", "Indonesia", "Japan", "Malaysia", "South Korea", "Taiwan", "Thailand", "Vietnam"]
+table = {}  
+
+for country in country_list:
+    a = EnergyData(country)
+    table[country] = a.get_the_share(country, 2022).rename(columns= {"Share, %": country}).drop([2022], axis=1)
+
+result = table[country_list[0]]
+
+for country in country_list[1:]:
+    result = pd.merge(result, table[country], left_index=True, right_index=True)
+
+result = result.reset_index()
+
+print(result)
+
+
+
+
+
+
 
