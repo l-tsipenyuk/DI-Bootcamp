958b81de2aa12a0a3468ad6daff9fe332f484493
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-09-28 19:00:31 +0300

    xp

--- Week4_comp_complex/Day2/ExercisesXP/xp.py
+++ Week4_comp_complex/Day2/ExercisesXP/xp.py
@@ -63,6 +63,26 @@
 
 # This class serves as the building block for creating your linked list.
 
+# Part 2: Create The LinkedList Class
+# Your next step is to create a LinkedList class. This class should contain:
+
+# An __init__ method that initializes a head attribute as None.
+# An append method to add new nodes to the end of the list:
+# Create a new node with the given value using the Node class.
+# If the head is None, make this new node the head.
+# Otherwise, traverse the list to find the last node and attach the new node there.
+# This class will serve as a wrapper around the Node objects and will offer a way to manipulate the linked list.
+
+# Part 3: Implement The Merge_sorted_linked_lists Function
+# Finally, write a function named merge_sorted_linked_lists that does the following:
+
+# Takes two sorted LinkedList objects as input arguments.
+# Initializes a “dummy” node to help easily build the new sorted linked list.
+# Uses two pointers to traverse both lists and a third pointer to construct the new list.
+# Adds the node with the smaller value to the new list and advances the respective list pointer.
+# If one list is exhausted, appends the remaining nodes from the other list to the new list.
+# The function should return the head of the new sorted linked list.
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -81,10 +101,43 @@ class LinkedList:
             while current_node.next:
                 current_node = current_node.next
             current_node.next = new_node
+    
+def merge_sorted_linked_lists(list_1, list_2):
+    merged_list = LinkedList()
+    current1 = list_1.head
+    current2 = list_2.head
 
-list_ex = LinkedList()
-list_ex.append(3)
-list_ex.append(2)
-list_ex.append(1)
+    while current1 is not None and current2 is not None:
+        if current1.value < current2.value:
+            merged_list.append(current1.value)
+            current1 = current1.next
+        else:
+            merged_list.append(current2.value)
+            current2 = current2.next
 
-print(list_ex)
+        while current1 is not None:
+            merged_list.append(current1.value)
+            current1 = current1.next
+        while current2 is not None:
+            merged_list.append(current2.value)
+            current2 = current2.next
+
+        return merged_list
+
+list_1 = LinkedList()
+list_1.append(3)
+list_1.append(2)
+list_1.append(1)
+
+list_2 = LinkedList()
+list_2.append(6)
+list_2.append(5)
+list_2.append(4)
+
+merged = merge_sorted_linked_lists(list_1, list_2)
+
+current = merged.head
+while current:
+    print(current.value, end = " ")
+    current = current.next
+
