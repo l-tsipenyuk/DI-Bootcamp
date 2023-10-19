8a7f7419f3bbc4c47e49025b3013a57fa8fab9c6
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-19 10:58:43 +0300

    lesson

---
+++ Week5/Day4/lesson/script.js
@@ -0,0 +1,81 @@
+// console.log("Hello World");
+
+// variables
+// let x = 3;
+
+// console.log("x");
+// console.log(x);
+
+// let y = 1;
+// let Y = 2;
+// console.log(y);
+// console.log(Y);
+
+// Camel Case (name of variables should start from lower letter and than from the capitals):
+// let userName="Daniel";
+// let myNameIs="Daniel";
+
+// Keywords:
+// let x,y;
+// console.log(x);
+
+// console.log("Like this")
+// console.log('Like this (single)');
+
+// let a = "Hello";
+// let b = "World";
+// let c = a + b;
+// console.log(c);
+
+// let x=1;
+// let y = 2;
+// console.log(x + y);
+
+// let longString = "This is a very long string which needs " +
+//     "to wrap across multiple lines because " +
+//     "otherwise, my code is unreadable.";
+
+// let longString2 = "This is a very long string which needs to wrap across multiple lines because otherwise, my code is unreadable.";
+
+// let stringLength=longString.length;
+// console.log(longString.length);
+// console.log(stringLength);
+
+//  how to get index of a word in string
+
+// let str = "Hello, please say hello to the class";
+// let upperCaseHello = str.indexOf("Hello")
+// console.log(upperCaseHello)
+
+// let lowerCaseHello = str.indexOf("hello")
+// console.log(lowerCaseHello)
+
+// let partOfString = str.substring(10,25);
+// console.log(partOfString);
+
+// let str = "Hello, Please Say Hello To The Class";
+// let toLower = str.toLowerCase();
+// console.log(toLower);
+
+
+// Exercise1:
+// let addressNumber = 6;
+// let addressStreet = "Hoofien ";
+// let country = "Israel";
+// let globalAddress = "I live in "+addressStreet + addressNumber + ", in " + country
+// let betterGlobalAddress = `I live in ${addressStreet} ${addressNumber}, in ${country}`;
+// console.log(globalAddress)
+// console.log(betterGlobalAddress)
+
+
+// Number
+// console.log(1*2);
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 / 2);
+
+// Number methods
+// let op = 'me'
+// let n = 1;
+// console.log(isNaN(op));
+// console.log(n.toString());
