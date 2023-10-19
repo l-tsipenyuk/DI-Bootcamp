f52ac1b197c383c83ac6d0ee3f2d5a9800038eb4
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-19 22:52:04 +0300

    xp

--- Week5/Day4/ExercisesXP/scriptXP.js
+++ Week5/Day4/ExercisesXP/scriptXP.js
@@ -19,18 +19,18 @@
 // console.log(maryInd);
 
 // // 1.5. Write code to make a copy of the people array using the slice method.
- 
+
 // let copy = people.slice(1,3);
 // console.log(copy);
 
-// // 1.6.Write code that gives the index of “Foo”. Why does it return -1 ? 
+// // 1.6.Write code that gives the index of “Foo”. Why does it return -1 ?
 // fooInd = people.indexOf("Foo");
 // console.log(fooInd);
 
 // It's -1 because there is no "Foo" in "people"
 
 // 1.7. Create a variable called last which value is the last element of the array.
-    // Hint: What is the relationship between the index of the last element in the array and the length of the array ?
+// Hint: What is the relationship between the index of the last element in the array and the length of the array ?
 
 // let last = people.slice(people.length-1, people.length);
 // console.log(last);
@@ -52,17 +52,51 @@
 //     }
 // }
 
+//  Exercise 2 : Your Favorite Colors
+// 2.1.Create an array called colors where the value is a list of your five favorite colors.
+// let colors = ['yellow', 'purple', 'blue', 'gold', 'black']
 
+// 2.2.Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+// for (let i of colors) {
+//     if (i == 'yellow') {
+//         console.log("My #1 choice is yellow.")
+//     }
+//     if (i == 'purple') {
+//         console.log("My #2 choice is purple.")
+//     }
+//     if (i == 'blue') {
+//         console.log("My #3 choice is blue.")
+//     }
+//     if (i == 'gold') {
+//         console.log("My #4 choice is gold.")
+//     }
+//     if (i == 'black') {
+//         console.log("My #5 choice is black.")
+//     }
+// }
 
+// 2.3.Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+// Hint : create an array of suffixes to do the Bonus
 
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(`My number ${i+1} choice is ${colors[i]}`);
+// }
 
+// Exercise 3 : Repeat The Question
+// 3.1.Prompt the user for a number.
+    // Hint : Check the data type you receive from the prompt(ie.Use the typeof method)
 
+// let userAge = prompt("How old are you?");
+// console.log(Number(userAge));
 
+// 3.2.While the number is smaller than 10 continue asking the user for a new number.
+    // Tip : Which while loop is more relevant for this situation ?
 
-
-
-
-
+// let userAge = prompt("How old are you?");
+// while (userAge <10) {
+//     userAge = prompt("Type another number larger than 10.");
+//     console.log(userAge);
+// }
 
 
 
