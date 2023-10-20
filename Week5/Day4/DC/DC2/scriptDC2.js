a36f204988c74f0b1fd2d15efa51768a93680e8d
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-20 14:41:33 +0300

    dc2

---
+++ Week5/Day4/DC/DC2/scriptDC2.js
@@ -0,0 +1,27 @@
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops(Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+
+
+// method 1
+// sentence = ' '
+// for (let i = 0; i < 6; i++) {
+//     sentence += '*';
+//     console.log(sentence);
+// }
+
+// method 2
+// for (let i = 1; i < 7; i++) {
+//     let sentence = '';
+//     for (let j = 1; j <= i; j ++) {
+//         sentence += '*';
+//     }
+//     console.log(sentence);
+// }
+
+
+
+
+
+
+
