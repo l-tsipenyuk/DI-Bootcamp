4d8d36cda326410e0b1d098aea77a6cfac55eb52
l-tsipenyuk <lia.tsipenyuk@gmail.com>
2023-10-25 12:31:14 +0300

    xp

---
+++ Week6/Day4/ExercisesXP/scriptxp.js
@@ -0,0 +1,23 @@
+// Exercise 1 : Colors
+// Instructions
+// Using this array:
+// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+// Write a JavaScript program that displays the colors in the following order: “1# choice is Blue.” “2# choice is Green.” “3# choice is Red.” ect…
+// Check if at least one element of the array is equal to the value “Violet”. If yes, console.log("Yeah"), else console.log("No...")
+// Hint: Use the array methods taught in class.Look at the lesson Array Methods.
+
+// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+// colors.forEach((element, index) => {
+//     console.log(`${index+1}# choice is ${element}.`)
+// });
+
+// let output = colors.some((element) =>element === "Violet");
+// let result = output? console.log("Yeah") : console.log("No...");
+
+// Exercise 2 : Colors #2
+// Instructions
+// Using these arrays:
+// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+// const ordinal = ["th", "st", "nd", "rd"];
+// Write a JavaScript program that displays the colors in the following order: “1st choice is Blue.” “2nd choice is Green.” “3rd choice is Red.” ect…
+// Hint: Use the array methods taught in class and ternary operator.
