2f95102a921745de8246daed154e9ce03d609cdd
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-11-06 14:56:55 +0200

    xp

--- Week8/Day2/ExercisesXP/blog-api/server.js
+++ Week8/Day2/ExercisesXP/blog-api/server.js
@@ -40,13 +40,49 @@ app.get('/data', (req,res)=>{
 
 // 1.9. GET / posts /: id: Return a specific blog post based on its id.
 
+app.get("/data/:id", (req, res) => {
+    const { id } = req.params;
+    const post = data.find((item) => item.id == id);
+    if (!post) return res.status(404).json({ message: "Product Not Found" });
+    res.json(post);
+});
 
+// 1.10. POST / posts: Create a new blog post.
 
+app.post("/data", (req, res) => {
+    console.log("body=>", req.body);
+    const new_post = { ...req.body, id: data.length + 1 };
+    data.push(new_post);
+    res.status(201).json(data);
+});
 
+// 1.11. PUT / posts /: id: Update an existing blog post.
 
+app.put('/data/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedPost = req.body;
+    const existingPost = data.find((item) => item.id == id);
+    if (existingPost) {
+        Object.assign(existingPost, updatedPost);
+        res.json(existingPost);
+    } else {
+        res.status(404).json({ message: 'Post not found' });
+    }
+});
 
+// 1.12. DELETE / posts /: id: Delete a blog post.
 
+app.delete('/data/:id', (req, res) => {
+    const { id } = req.params;
+    const indx = data.findIndex((item) => item.id == id);
+    if (indx === -1) return res.sendStatus(404); 
+    const deleted = data.splice(indx, 1);
+    res.sendStatus(201);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// 1.13. Implement error handling for invalid routes and server errors.
+// 1.14. Start the Express app and listen on a specified port(e.g., 3000).
