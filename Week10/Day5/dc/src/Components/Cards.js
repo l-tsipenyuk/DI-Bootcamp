89928fd207beb5d61ea84b876127c39dc3f74e1a
ltsipenyuk <lia.tsipenyuk@gmail.com>
2023-11-25 00:34:56 +0200

    dc

--- Week10/Day5/dc/src/Components/Cards.js
+++ Week10/Day5/dc/src/Components/Cards.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 
 const Cards = (props) => {
     const [cards, setCards] = useState([]);
+    const [count, setCount] = useState(0);
+    const [clickedCards, setClickedCards] = useState([]);
+    const [topScore, setTopScore] = useState([]);
 
     useEffect(() => {
         const showCards = async () => {
@@ -25,20 +28,34 @@ const Cards = (props) => {
         shuffleCards();
     }
 
+    const counter = () => {
+        setCount(count + 1);
+    }
+
+    const handleCounter = (id) => {
+        if(clickedCards.includes(id)){
+            setCount(0);
+            setClickedCards([]);
+            setTopScore(count);
+        }else{
+            counter();
+            setClickedCards([...clickedCards, id]);
+        }
+    }
 
     return (
         <>
             <div className="header">
                 <h1>Superheroes Memory Game</h1>
-                <h4>Score: Top Score:</h4>
+                <h4>Score: {count} Top Score: {topScore}</h4>
             </div>
             <h3>Get points by clicking on an image but don't click on them more than once!</h3>
             <div className="container" onClick={handleShuffle}>
                 {cards.map((item) => (
-                    <div className="heroCard" key={item.id}>
+                    <div className="heroCard" key={item.id} onClick={()=>handleCounter(item.id)}>   
                         < img src={item.image} alt="Not found" />
-                        <h5>{item.name}</h5>
-                        <h5>{item.occupation}</h5>
+                        <h5><i>Name:</i> {item.name}</h5>
+                        <h5><i>Occupation:</i> {item.occupation}</h5>
                     </div>
                 ))}
             </div >
